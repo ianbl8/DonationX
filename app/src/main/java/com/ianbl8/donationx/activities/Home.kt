bfbb9461e40f61b53a6062d622a6204b0588b7cf
Ian Blake <ian@ianbl8.com>
2023-12-16 19:46:45 +0000

    Add floating action button

--- app/src/main/java/com/ianbl8/donationx/activities/Home.kt
+++ app/src/main/java/com/ianbl8/donationx/activities/Home.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.*
 import androidx.navigation.ui.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.ianbl8.donationx.R
 import com.ianbl8.donationx.databinding.HomeBinding
 
@@ -23,9 +25,21 @@ class Home : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
         val navController = findNavController(R.id.nav_host_fragment)
 
-        appBarConfiguration = AppBarConfiguration(setOf(R.id.donateFragment, R.id.reportFragment, R.id.aboutUsFragment), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.donateFragment,
+                R.id.reportFragment,
+                R.id.aboutUsFragment
+            ), drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         val navView = homeBinding.navView
